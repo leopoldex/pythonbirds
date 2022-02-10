bf805cc7f9b58e75af08ef22d977d8a28a9628bb
Leopoldo <leopoldo.pavan@hotmail.com>
2022-02-09 22:44:56 -0300

    Criados atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,16 +7,21 @@ Ao definir um método ou função é importante ser tudo minúsculo e palavras s
 '''
 
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('leopoldo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
+    leopoldo = Pessoa(nome='leopoldo')
+    joao = Pessoa(nome='João')
+
+    luciano = Pessoa(leopoldo, joao, nome='Liciano')
+    print(leopoldo.cumprimentar())
+    print(leopoldo.nome)
+    print(leopoldo.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
