1191c205406037d129dceb5b67bcc86b7b662df2
Leopoldo <leopoldo.pavan@hotmail.com>
2022-02-09 22:16:12 -0300

    Criada a classe pessoa.py

---
+++ oo/pessoa.py
@@ -0,0 +1,9 @@
+'''
+Segundo a PEP8 o nome de uma classe deve começar com letra maiúscula
+e as demais palavras devem ser íniciadas com a letra maiícula também
+Ex: ExemploPessoa, AtualizarTela etc)
+'''
+
+class Pessoa:
+    pass
+
