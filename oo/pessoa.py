76af5949e69159b0a85b246575b9d925bb7316cc
Leopoldo <leopoldo.pavan@hotmail.com>
2022-02-09 22:27:30 -0300

    Criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,8 +2,16 @@
 Segundo a PEP8 o nome de uma classe deve começar com letra maiúscula
 e as demais palavras devem ser íniciadas com a letra maiícula também
 Ex: ExemploPessoa, AtualizarTela etc)
+
+Ao definir um método ou função é importante ser tudo minúsculo e palavras separadas por underline
 '''
 
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
