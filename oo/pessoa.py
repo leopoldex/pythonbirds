08b4ff92a995eb20e5452e3d7a7b38388443cc78
Leopoldo <leopoldo.pavan@hotmail.com>
2022-02-09 22:34:58 -0300

    Criados atributos de Instância nome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,11 +7,16 @@ Ao definir um método ou função é importante ser tudo minúsculo e palavras s
 '''
 
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('leopoldo')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
