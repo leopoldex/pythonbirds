96979ba37a4dd7fbb654409010c41b74ca6f982d
Leopoldo <leopoldo.pavan@hotmail.com>
2022-02-15 21:25:20 -0300

    Criados atributos olhos como atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,6 +7,12 @@ Ao definir um método ou função é importante ser tudo minúsculo e palavras s
 '''
 
 class Pessoa:
+    olhos = 2
+    ''' atributo de classe, default, que será igual para todos os objetos. 
+                 Declarando dessa maneira o python não precisa alocar varios espaços de 
+                 memória de valores iguais para objetos diferentes. Dessa forma é possível acessar
+                 o valor de olhos diretamente na classe e também nos objetos'''
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -25,7 +31,11 @@ if __name__ == '__main__':
     for filho in luciano.filhos:
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
+    luciano.olhos = 1
+    del luciano.olhos
     del luciano.sobrenome
-    print(luciano.__dict__) #exibe todos os atributos do objeto
+    print(luciano.__dict__) #exibe todos os atributos de instância e não os atributos de classe. Apenas quando houver alteração direta do atributo ele aparecerá aqui.
     print(leopoldo.__dict__)
-
+    print(Pessoa.olhos)
+    print(leopoldo.olhos)
+    print(luciano.olhos)
